fe389411481f208da325d09e15b2521a6197d356
Rizky Dhani <rizkydhni03@gmail.com>
2026-06-25 14:53:52 +0700

    fix(temp-deviation): restructure Period/Initial/Grifols into 3-column layout
    - Period: col-6 with border (no right border)
    - Initial name: col-3 no border (text only)
    - Grifols: col-3 with border
    - Applied to both main PDF template and header preview

--- resources/views/print/temperature-deviation.blade.php
+++ resources/views/print/temperature-deviation.blade.php
@@ -161,16 +161,12 @@ $renderGroups =
                                 </div>
                             </div>
                         </div>
-                        <div class="col-6 border border-black py-1 px-0">
-                            <div class="row h-100">
-                                <div class="col-6 border-end border-black d-flex align-items-center py-1 ps-1">
-                                    <p class="mb-0" style="font-size: 11px;">**) Initial name and date (<i>Inisial nama dan tanggal</i>)</p>
-                                </div>
-                                <div class="col-6 d-flex align-items-center py-1 px-1">
-                                    <p class="fw-bolder text-center mb-0" style="font-size: 9px;">Specifically for Grifols Diagnostics products, immediately report to QA and Grifols Diagnostics principals if there is a temperature deviation <br><br>
-                                        (Khusus produk Grifols Diagnostics, segera laporkan ke QA dan prinsipal Grifols Diagnostics jika terjadi penyimpangan suhu)</p>
-                                </div>
-                            </div>
+                        <div class="col-3 d-flex align-items-center py-1 px-2">
+                            <p class="mb-0" style="font-size: 11px;">**) Initial name and date (<i>Inisial nama dan tanggal</i>)</p>
+                        </div>
+                        <div class="col-3 border border-black py-1 px-1">
+                            <p class="fw-bolder text-center mb-0" style="font-size: 9px;">Specifically for Grifols Diagnostics products, immediately report to QA and Grifols Diagnostics principals if there is a temperature deviation <br><br>
+                                (Khusus produk Grifols Diagnostics, segera laporkan ke QA dan prinsipal Grifols Diagnostics jika terjadi penyimpangan suhu)</p>
                         </div>
                     </div>
 
