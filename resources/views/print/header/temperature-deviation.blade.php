ad23e2f53f5b2731867e5932566ddf1f4ee42250
Rizky Dhani <rizkydhni03@gmail.com>
2026-06-25 15:07:17 +0700

    fix(temp-deviation): add border to Initial column, adjust to col-6/col-3/col-3
    - Period: col-6 with border (no right border)
    - Initial: col-3 with border (was borderless)
    - Grifols: col-3 with border (no left border)
    - Prevents double borders between adjacent columns

--- resources/views/print/header/temperature-deviation.blade.php
+++ resources/views/print/header/temperature-deviation.blade.php
@@ -56,7 +56,7 @@
             </div>
         </div>
         <div class="row mb-2" id="detail">
-            <div class="col-5 border border-black border-end-0 py-1">
+            <div class="col-6 border border-black border-end-0 py-1">
                 <div class="row">
                     <div class="col-3">
                         <p class="mb-0" style="font-size: 16px;">Period (<i>Periode</i>)</p>
@@ -70,10 +70,10 @@
                     </div>
                 </div>
             </div>
-            <div class="col-3 d-flex align-items-center py-1 px-2">
+            <div class="col-3 border border-black py-1 px-2">
                 <p class="mb-0" style="font-size: 16px;">**) Initial name and date (<i>Inisial nama dan tanggal</i>)</p>
             </div>
-            <div class="col-4 border border-black py-1 px-1">
+            <div class="col-3 border border-black border-start-0 py-1 px-1">
                 <p class="fw-bolder text-center mb-0" style="font-size: 10px;">Specifically for Grifols Diagnostics products, immediately report to QA and Grifols Diagnostics principals if there is a temperature deviation <br><br>
                     (Khusus produk Grifols Diagnostics, segera laporkan ke QA dan prinsipal Grifols Diagnostics jika terjadi penyimpangan suhu)</p>
             </div>
